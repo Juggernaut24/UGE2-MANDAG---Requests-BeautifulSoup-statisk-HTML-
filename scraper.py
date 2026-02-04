f0bbbd32efa47b1ff35a9afebd715220cd884d50
Mehmet Mert Altunay <109654423+Juggernaut24@users.noreply.github.com>
2026-02-04 10:41:35 +0100

    Update scraper.py
    Made a basic start code for scraping.

--- scraper.py
+++ scraper.py
@@ -1,2 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+import json
 import os
 
+os.makedirs('outputs', exist_ok = True)
+
+target_url = "https://quotes.toscrape.com/"
+
+response = requests.get(target_url)
+print(response.status_code)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+#print(response.content)
+
+print(soup)
